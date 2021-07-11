e95d1803945964ea174e652acdfa6cfcf63279ed
wrmacdonald <wrmacdonald2016@gmail.com>
2021-07-10 17:19:12 -0700

    add j2 layout and home & local test functionality

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask("Pawsome")
 
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return "Hello World"
+    return render_template('nw57_home.j2')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 3000))
+    app.run(port=port, debug=True)
