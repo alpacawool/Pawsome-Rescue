00209e163aefba279d3915bad1987a489e590278
wrmacdonald <wrmacdonald2016@gmail.com>
2021-07-12 13:55:15 -0700

    add pet_profile route

--- app.py
+++ app.py
@@ -5,12 +5,20 @@ app = Flask("Pawsome")
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template('nw57_home.j2')
 
 @app.route("/index")
 def index_page():
     return render_template('nw57_index.j2')
+
+@app.route("/animals")
+def animals():
+    return render_template('nw57_animals.j2')
+
+@app.route("/animals/<int:animal_id>")
+def pet_profile(animal_id):
+    return (render_template('nw57_pet_profile.j2'), animal_id)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 3000))
