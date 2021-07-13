656854982157895653271f5a55666c4a6ac80262
Patricia Booth <44758340+alpacawool@users.noreply.github.com>
2021-07-12 23:45:49 -0700

    Added Edit User Page

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 import os
 
+# Testing with User Object Below
+from test_object.test_users import User, test_users
+users = test_users
+# End Testing Content Above
+
 app = Flask("Pawsome")
+app.secret_key = "1234" # For flask flash
 
 @app.route("/")
 @app.route("/home")
@@ -12,13 +18,30 @@ def home():
 def index_page():
     return render_template('nw57_index.j2')
 
-@app.route("/animals")
-def animals():
-    return render_template('nw57_animals.j2')
 
 @app.route("/settings")
 def settings():
     return render_template('nw57_settings.j2')
+
+@app.route("/edit-users")
+def edit_users():
+    return render_template('nw57_edit_users.j2', users=users)
+
+@app.route("/update-user/<int:user_id>", methods=['GET', 'POST'])
+def update_user(user_id):
+    if request.method == 'POST':
+        req = request.form
+        for user in users:
+            if user.id == user_id:
+                user.first = req["user_first"]
+                user.last = req["user_last"]
+                user.email = req["user_email"]
+                flash("Updated user successfully!")
+    return redirect("/edit-users")
+
+@app.route("/animals")
+def animals():
+    return render_template('nw57_animals.j2')
 
 @app.route('/animals/<int:animal_id>', methods=['GET', 'POST'])
 def pet_profile(animal_id):
