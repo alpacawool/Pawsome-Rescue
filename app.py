8704d857eee27d4233d249eae5bcdfeff86e20aa
wrmacdonald <wrmacdonald2016@gmail.com>
2021-07-12 21:13:24 -0700

    add add_animal route, update_animal in progress

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import os
 
 app = Flask("Pawsome")
@@ -23,6 +23,45 @@ def pet_profile(animal_id):
         return 'Submit Application'
     else:
         return render_template('nw57_pet_profile.j2', animal_id=animal_id)
+
+
+@app.route("/add_animal", methods=['GET', 'POST'])
+def add_animal():
+    if request.method == 'GET':
+        shelterQueryResult = None
+        # TODO: add SELECT names from Shelters DB Logic
+        return render_template('nw57_add_animal.j2', shelters = shelterQueryResult)
+
+    elif request.method == 'POST': 
+        animalName = request.form['animalName']
+        shelterName = request.form['shelterName']
+        birthdate = request.form['birthdate']
+        gender = request.form['gender']
+        speciesType = request.form['speciesType']
+        breed = request.form['breed']
+        personality = request.form['personality']
+        imageURL = request.form['imageURL']
+        intakeDate = request.form['intakeDate']
+        if request.form['adoptedDate']:
+            adoptedDate = request.form['adoptedDate']
+        else:
+            adoptedDate = None
+        adoptionFee = request.form['adoptionFee']
+
+        # TODO: add POST to DB Logic
+
+        print("Adding New Animal:", animalName)
+        return redirect(url_for('update_animals'))
+
+
+@app.route("/update_animal/<int:animal_id>", methods=['GET', 'POST'])
+def update_animals():
+    if request.method == 'POST': 
+        # add Update to DB Logic
+        return render_template('nw57_update_animals.j2')
+    else:
+        return render_template('nw57_update_animals.j2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 3000))
