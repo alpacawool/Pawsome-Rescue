31d3a48bacdee71797db9f7b9c3f582f089908dd
Patricia Booth <44758340+alpacawool@users.noreply.github.com>
2021-07-21 12:39:38 -0700

    Added semicolons

--- database/nw57_db_DMQ.sql
+++ database/nw57_db_DMQ.sql
@@ -6,57 +6,57 @@
 -- Add Animals Page
 --  INSERT Animals
 INSERT INTO Animals(shelter_id, animal_name, birthdate, gender, species_type, breed, personality, image_url, intake_date, adopted_date, adoption_fee)
-VALUES (:shelter_id_from_shelter_SELECTion, :animal_name, :birthdate, :gender, :species_type, :breed, :personality, :image_url, :intake_date, :adopted_date, :adoption_fee)
+VALUES (:shelter_id_from_shelter_SELECTion, :animal_name, :birthdate, :gender, :species_type, :breed, :personality, :image_url, :intake_date, :adopted_date, :adoption_fee);
 --  SELECT Shelters to populate Shelter Name Selection
 SELECT id, shelter_name
-FROM Shelters
+FROM Shelters;
 
 -- Update Animals Page
 --  UPDATE Animals
 UPDATE Animals 
 SET shelter_id = :shelter_id_from_shelter_SELECTion, animal_name = :animal_name, birthdate = :birthdate, gender = :gender, species_type = :species_type, breed = :breed, personality = :personality, image_url = :image_url, intake_date = :intake_date, adopted_date = :adopted_date, adoption_fee = :adoption_fee
-WHERE id = :id_of_animal_to_be_updated
+WHERE id = :id_of_animal_to_be_updated;
 --  SELECT Shelters to populate Shelter Name Selection
 SELECT id, shelter_name
-FROM Shelters
+FROM Shelters;
 
 -- Filter Animals Page
 --  SELECT all Animals
 SELECT Animals.id, shelter_id, animal_name, birthdate, gender, species_type, breed, personality, image_url, intake_date, adopted_date, adoption_fee, Shelters.id, shelter_name
 FROM Animals 
 INNER JOIN Shelters ON shelter_id = Shelters.id
-ORDER BY intake_date ASC
+ORDER BY intake_date ASC;
 --  SELECT subset of Animals - based on the Species Type Filter
 SELECT Animals.id, shelter_id, animal_name, birthdate, gender, species_type, breed, personality, image_url, intake_date, adopted_date, adoption_fee, Shelters.id, shelter_name
 FROM Animals 
 INNER JOIN Shelters ON shelter_id = Shelters.id
 WHERE species_type = :species_type_from_the_filter_dropdown
-ORDER BY intake_date ASC
+ORDER BY intake_date ASC;
 --  SELECT subset of Animals - based on the Shelter Name Filter
 SELECT Animals.id, shelter_id, animal_name, birthdate, gender, species_type, breed, personality, image_url, intake_date, adopted_date, adoption_fee, Shelters.id, shelter_name
 FROM Animals 
 INNER JOIN Shelters ON shelter_id = Shelters.id
 WHERE shelter_name = :shelter_name_from_the_filter_dropdown
-ORDER BY intake_date ASC
+ORDER BY intake_date ASC;
 --  SELECT subset of Animals - based on the Available Filter - Available
 SELECT Animals.id, shelter_id, animal_name, birthdate, gender, species_type, breed, personality, image_url, intake_date, adopted_date, adoption_fee, Shelters.id, shelter_name
 FROM Animals 
 INNER JOIN Shelters ON shelter_id = Shelters.id
 WHERE adopted_date IS NULL
-ORDER BY intake_date ASC
+ORDER BY intake_date ASC;
 --  SELECT subset of Animals - based on the Available Filter - Adopted
 SELECT Animals.id, shelter_id, animal_name, birthdate, gender, species_type, breed, personality, image_url, intake_date, adopted_date, adoption_fee, Shelters.id, shelter_name
 FROM Animals 
 INNER JOIN Shelters ON shelter_id = Shelters.id
 WHERE adopted_date IS NOT NULL
-ORDER BY intake_date ASC
+ORDER BY intake_date ASC;
 
 -- Pet Profile Pages (available from "Learn More About Me" links on Filter Animals Page)
 -- SELECT single pet by animal_id
 SELECT Animals.id, shelter_id, animal_name, birthdate, gender, species_type, breed, personality, image_url, intake_date, adopted_date, adoption_fee, Shelters.id, shelter_name
 FROM Animals 
 INNER JOIN Shelters ON shelter_id = Shelters.id
-WHERE Animals.id = :id_of_desired_animal
+WHERE Animals.id = :id_of_desired_animal;
 -- INSERT Application
 
 
@@ -64,14 +64,14 @@ WHERE Animals.id = :id_of_desired_animal
 -- Edit Shelters Page
 --  INSERT Shelters
 INSERT INTO Shelters(shelter_name, street, city, state, zip_code)
-VALUES (:shelter_name, :street, :city, :state, :zip_code)
+VALUES (:shelter_name, :street, :city, :state, :zip_code);
 --  DELETE Shelters
-DELETE FROM Shelters WHERE id = :id_of_shelter_to_be_deleted
+DELETE FROM Shelters WHERE id = :id_of_shelter_to_be_deleted;
 
 -- View Shelters Page
 --  SELECT Shelters
 SELECT id, shelter_name, street, city, state, zip_code
-FROM Shelters
+FROM Shelters;
 
 -- Applications
 -- Edit Applications Page
